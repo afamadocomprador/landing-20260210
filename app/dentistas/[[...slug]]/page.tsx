2523227fbc0c80ef868ed782530893ef4b83af79
afamadocomprador <165040121+afamadocomprador@users.noreply.github.com>
2026-02-10 14:48:55 +0100

    Update page.tsx

--- app/dentistas/[[...slug]]/page.tsx
+++ app/dentistas/[[...slug]]/page.tsx
@@ -23,7 +23,7 @@ export default async function DentistasPage({ params }: PageProps) {
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    { cookies: { get: (name) => cookieStore.get(name)?.value } }
+    { cookies: { get: (name: string) => cookieStore.get(name)?.value } }
   );
 
   const currentSlug = params.slug && params.slug.length > 0 ? params.slug[0] : "";
